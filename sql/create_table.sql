6cd55765dc7b9357e2430338e992746b0d875eaa
Dong PV <dongpv@vietnamlab.vn>
2020-09-21 17:07:30 +0700

    remove test code

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,5 +1,5 @@
 -- spider node
-CREATE TABLE spider.sp_test2
+CREATE TABLE spider.sp_test
 (
     id int unsigned NOT NULL AUTO_INCREMENT
     , name char(120) NOT NULL DEFAULT ''
@@ -9,19 +9,10 @@ ENGINE=spider
 COMMENT='wrapper "mysql", table "sp_test"'
 PARTITION BY KEY (id)
 (
-PARTITION dn11 COMMENT = 'srv "data_node1"'
-, PARTITION dn22 COMMENT = 'srv "data_node2"'
-, PARTITION dn33 COMMENT = 'srv "data_node3"'
+PARTITION dn1 COMMENT = 'srv "data_node1"'
+, PARTITION dn2 COMMENT = 'srv "data_node2"'
+, PARTITION dn3 COMMENT = 'srv "data_node3"'
 );
-
-
-CREATE TABLE spider.test
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50)
-) ENGINE=Spider
-    COMMENT='wrapper "mysql", srv "data_node1", table "sp_test"';
-
 
 
 
